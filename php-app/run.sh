93395ec0140b2187b85d516496b890bfc9574c68
otokawa-tatsumi <kiyotaka.otokawa@tatsu-mi.co.jp>
2024-04-30 16:30:07 +0900

    .envは含めないように変更

--- php-app/run.sh
+++ php-app/run.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+# .env作成
+ENV_FILE=".env"
+EXAMPLE_FILE=".env.example"
+
+# Check if .env file already exists
+if [ ! -f "$ENV_FILE" ]; then
+    # Copy .env.example to .env
+    cp "$EXAMPLE_FILE" "$ENV_FILE"
+    echo ".env file created."
+else
+    echo ".env file already exists."
+fi
+
 # パッケージインストール
 composer install --no-dev --optimize-autoloader
 
